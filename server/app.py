238a1707eb4b677f24499aff8b2a18ff498ad62f
Misran Ablikem Dolan <kelnetic.info@gmail.com>
2024-07-12 14:47:12 -0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -104,10 +104,6 @@ def ingest_documents(dataset: Dataset):
 @app.post("/chat")
 def chat(message: Message):
     response = uc_core.chat_engine.chat(messages=[UserMessage(content=message.content)])
-    #TODO: Check the response choices and see what message is the best, should it be the longest message? The one with the most statistics? Mentions of genocide?
-    #Can create a custom instance of query generator with a defualt system prompt that might be able to add more queries
-    #I think in the query generator, can include a sentence to not send a function if it asking what the purpose of the chatbot is, or who "you" are
-    # print(response)
     content = response.choices[0].message.content
     context_values = set()
     context = []
